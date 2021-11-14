cd4a46531b7b03f29258deb940a724843d9c4819
Stacy Read <sread@wctc.edu>
2021-11-13 20:12:50 -0600

    Pancake demo for logical operators

---
+++ module5/5.3-pancakes/pancakes.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+    $("button").click(checkShopping);
+});
+
+function checkShopping() {
+    let wantPancakes = $("input#pancakes").is(":checked");
+    let hasButter = $("input#butter").is(":checked");
+    let hasSyrup = $("input#syrup").is(":checked");
+
+    let goToStore = false;
+
+    if (wantPancakes) {
+        if (!hasButter) {
+            goToStore = true;
+        }
+        if (!hasSyrup) {
+            goToStore = true;
+        }
+    }
+
+    $("p#nested").text(goToStore);
+
+    // Reset for next test
+    goToStore = false;
+
+    // Alternative:
+    // let outOfButter = !hasButter;
+    // let outOfSyrup = !hasSyrup;
+
+    if (wantPancakes && !hasButter || !hasSyrup) {
+        goToStore = true;
+    }
+
+    $("p#noPar").text(goToStore);
+
+    // Reset for next test
+    goToStore = false;
+
+    // Alternative:
+    // if (isMakingPancakes && !(hasButter && hasSyrup))
+
+    if (wantPancakes && (!hasButter || !hasSyrup)) {
+        goToStore = true;
+    }
+
+    $("#withPar").text(goToStore);
+}
